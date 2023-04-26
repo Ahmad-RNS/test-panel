0bb5e6af5511a396e6dae81801fa54bb189d7ba4
Ahmad <ahmad.nawaz@rnssol.com>
2023-04-26 11:55:40 +0500

    basic-template

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,17 @@
+self.__RSC_CSS_MANIFEST={
+  "cssImports": {
+    "/home/asad/Documents/Workspace/whitelable-AdminPanel/git_panel/src/app/page.tsx": [
+      "/home/asad/Documents/Workspace/whitelable-AdminPanel/git_panel/node_modules/next/font/google/target.css?{\"path\":\"src/app/page.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ],
+    "/home/asad/Documents/Workspace/whitelable-AdminPanel/git_panel/src/app/layout.tsx": [
+      "/home/asad/Documents/Workspace/whitelable-AdminPanel/git_panel/node_modules/next/font/google/target.css?{\"path\":\"src/app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}",
+      "/home/asad/Documents/Workspace/whitelable-AdminPanel/git_panel/src/app/globals.css"
+    ]
+  },
+  "cssModules": {
+    "/home/asad/Documents/Workspace/whitelable-AdminPanel/git_panel/src/app/page": [
+      "/home/asad/Documents/Workspace/whitelable-AdminPanel/git_panel/src/app/globals.css",
+      "/home/asad/Documents/Workspace/whitelable-AdminPanel/git_panel/node_modules/next/font/google/target.css?{\"path\":\"src/app/layout.tsx\",\"import\":\"Inter\",\"arguments\":[{\"subsets\":[\"latin\"]}],\"variableName\":\"inter\"}"
+    ]
+  }
+}
